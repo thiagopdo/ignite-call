dedb0277d24ffb4c627e98de3071a7ce417066d1
thiagopdo <thiago.falconer@gmail.com>
2024-11-20 18:20:56 -0300

    first commit

---
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -0,0 +1,62 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Text, TextInput } from "@ignite-ui/react";
+import { useRouter } from "next/router";
+import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Form, FormAnnotation } from "./styles";
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Mínimo de 3 caracteres" })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: "Apenas letras e hífens são permitidos",
+    })
+    .transform((username) => username.toLowerCase()),
+});
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>;
+
+export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  });
+
+  const router = useRouter();
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    const { username } = data;
+
+    await router.push(`/register/?username=${username}`);
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          crossOrigin={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuário"
+          {...register("username")}        />
+
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : "Digite o nome de usuário desejado"}
+        </Text>
+      </FormAnnotation>
+    </>
+  );
+}
---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,87 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
+import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Container, Form, FormError, Header } from "./styles";
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Mínimo de 3 caracteres" })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: "Apenas letras e hífens são permitidos",
+    })
+    .transform((username) => username.toLowerCase()),
+  name: z.string().min(3, { message: "Mínimo de 3 caracteres" }),
+});
+
+type RegisterFormData = z.infer<typeof registerFormSchema>;
+
+export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  });
+
+  async function handleRegister(data: RegisterFormData) {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Para começar a usar o Ignite Call, você precisa se registrar. Preencha
+          o formulário abaixo para criar sua conta.
+        </Text>
+
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+
+      <Form as="form" onSubmit={handleSubmit(handleRegister)}>
+        <label>
+          <Text size="sm">Nome de usuário</Text>
+          <TextInput
+            crossOrigin={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+            prefix="ignite.com/"
+            placeholder="seu-usuário"
+            {...register("username")}
+          />
+
+          {errors.username && (
+            <FormError size="sm">{errors.username.message}</FormError>
+          )}
+        </label>
+
+        <label>
+          <Text size="sm">Nome completo</Text>
+          <TextInput
+            crossOrigin={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+            prefix="ignite.com/"
+            placeholder="seu nome"
+            {...register("name")}
+          />
+
+          {errors.name && (
+            <FormError size="sm">{errors.name.message}</FormError>
+          )}
+        </label>
+
+        <Button type="submit">
+          Próximo
+          <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  );
+}
