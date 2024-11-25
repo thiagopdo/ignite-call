3375c57762b45db85920609453783c62b51c8192
thiagopdo <thiago.falconer@gmail.com>
2024-11-24 23:35:07 -0300

    feat: added nextauth and prisma adapter

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,14 +1,22 @@
 import { Button, Heading, MultiStep, Text } from "@ignite-ui/react";
 
-import { ArrowRight } from "phosphor-react";
+import { ArrowRight, Check } from "phosphor-react";
 
+import { signIn, useSession } from "next-auth/react";
 import { Container, Header } from "../styles";
-import { ConnectBox, ConnectItem } from "./styles";
+import { AuthError, ConnectBox, ConnectItem } from "./styles";
+import { useRouter } from "next/router";
 
 export default function Register() {
-  // async function handleRegister(data: RegisterFormData) {}
+  const session = useSession();
+  const router = useRouter();
 
-  AULA 19!!!
+  const hasAuthError = !!router.query.error;
+  const isSginedIn = session.status === "authenticated";
+
+  async function handleConnectCalendar() {
+    await signIn("google");
+  }
 
   return (
     <Container>
@@ -25,16 +33,33 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
-            Conectar
-            <ArrowRight />
-          </Button>
+          {isSginedIn ? (
+            <Button size="sm" disabled>
+              Conectado <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
         </ConnectItem>
 
-      <Button type="submit">
-        Próximo
-        <ArrowRight />
-      </Button>
+        {hasAuthError && (
+          <AuthError size="sm">
+            Falha ao se conectar ao Google. Por favor, verifique novamente as
+            permissões de acesso.
+          </AuthError>
+        )}
+
+        <Button type="submit" disabled={!isSginedIn}>
+          Próximo
+          <ArrowRight />
+        </Button>
       </ConnectBox>
     </Container>
   );
